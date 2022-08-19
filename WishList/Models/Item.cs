7138e81137b3f10722d76a5c21c68d345d40d0b4
Courtney Frey <courtneyb.frey@gmail.com>
2022-08-19 16:46:46 -0500

    models and validation created. DbContext updated

---
+++ WishList/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models
+{
+    public class Item
+    {
+        public int Id;
+
+        [Required]
+        [MaxLength(50)]
+        public string Description;
+
+
+        public Item()
+        {
+        }
+    }
+}
